1340820482373027334b3e1b748be3a10aee29a9
Stanisław Rudnicki <rudnicki@list.ru>
2020-08-28 22:38:09 +0300

    Commit

---
+++ WPF01_1_t01/README.md
@@ -0,0 +1,9 @@
+# Тема: StackPanel, WrapPanel, DockPanel
+
+Створіть інтерфейс додатку за поданим зразком, використовуючи XAML
+
+![ScreenShot](ScreenShot01.png)
+
+# Результат
+
+![ScreenShot](ScreenShot02.png)
---
+++ WPF01_2_t01/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF01_2_t01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string[] arr = ("Navy, Blue, Aqua, Teal, Olive, Green, Lime, Yellow, Orange, " +
+                "Red, Maroon, Fuchsia, Purple, Black, Silver, Gray, White")
+                .Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in arr)
+            {
+                var button = new Button { Content = item };
+                button.Foreground = (SolidColorBrush)new BrushConverter().ConvertFromString(item);
+                button.Margin = new Thickness(2);
+                wrapPanel1.Children.Add(button);
+            }
+        }
+    }
+}

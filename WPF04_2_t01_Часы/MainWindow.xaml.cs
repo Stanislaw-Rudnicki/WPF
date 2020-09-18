2d08fe3ca06629a84e4aaad872d1fc2161524555
Stanisław Rudnicki <rudnicki@list.ru>
2020-09-18 18:53:17 +0300

    Commit

---
+++ WPF04_2_t01_Часы/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+
+namespace WPF04_2_t01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(500);
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Time = DateTime.Now;
+        }
+
+        public DateTime Time
+        {
+            get { return (DateTime)GetValue(TimeProperty); }
+            set { SetValue(TimeProperty, value); }
+        }
+        public static readonly DependencyProperty TimeProperty =
+            DependencyProperty.Register("Time", typeof(DateTime), typeof(MainWindow), new PropertyMetadata(DateTime.Now));
+
+    }
+}
---
+++ WPF04_2_t01_Часы/README.md
@@ -0,0 +1,9 @@
+# Тема: DependencyProperty
+
+Використовуючи DependencyProperty створити годинник за поданим зразком.
+
+![ScreenShot](ScreenShot01.png)
+
+# Результат
+
+![ScreenShot](ScreenShot02.png)
